e1810a6a02d4a859534699956406422654b88e0a
fengmk2 <m@fengmk2.com>
2016-03-07 23:50:59 +0800

    fix fail test

--- git.sh
+++ git.sh
@@ -8,9 +8,10 @@ for name in $( cat repos.txt ); do
     project="tmp/${name}"
     rm -rf ${project}
     mkdir -p ${project}
+    echo "git clone https://github.com/${name}.git --depth=1 ${project}"
     git clone https://github.com/${name}.git --depth=1 ${project}
     cd ${project}
-    ${npminstall}
+    time ${npminstall} || exit $?
     time npm test || exit $?
     cd ${ROOT}
     rm -rf tmp
